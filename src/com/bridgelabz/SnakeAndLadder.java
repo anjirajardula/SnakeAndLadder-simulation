df17698fbb698edadfe34146671ef8296a055121
anjirajardula <anjirajardula@gmail.com>
2021-09-23 16:32:27 +0530

    Added:welcomr to snake and ladder program

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+/**
+ * purpose- To implement Snake And Ladder simulation using JAVA
+ *
+ * @author :Anjiraj
+ * @version :16.0
+ * @since :23/09/2021
+ */
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the snake and ladder program");
+    }
+
+}
